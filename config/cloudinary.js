baf24fbdea575a4545c3f4937c02f39ca2714054
Ijas Ahammed <83322316+ijas9118@users.noreply.github.com>
2025-11-01 14:31:51 +0530

    update cloudinary

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,10 +1,10 @@
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
 
 cloudinary.config({
-  cloud_name: 'dwrdo2jc3',
+  cloud_name: process.env.CLOUDINARY_API_KEY,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+});
 
 module.exports = cloudinary;
